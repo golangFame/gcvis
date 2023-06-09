ec5d713a061e8b4dd5efe14148a91401405e6238
Hiro <laciferin@gmail.com>
2023-06-09 09:01:51 +0530

    include: html files during compile

--- template.go
+++ template.go
@@ -1,25 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-)
+import _ "embed"
 
+//go:embed template.html
 var GCVIS_TMPL string
 
-func init() {
-	GCVIS_TMPL = readTemplateFile()
-}
-
-func readTemplateFile() (htmlString string) {
-	filePath := "template.html"
-	htmlBytes, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		log.Fatal(fmt.Errorf("Failed to read HTML file: %v", err))
-		return
-	}
-
-	htmlString = string(htmlBytes)
-	return
-}
+//func init() {
+//	GCVIS_TMPL = readTemplateFile()
+//}
+//
+//func readTemplateFile() (htmlString string) {
+//	filePath := "template.html"
+//	htmlBytes, err := ioutil.ReadFile(filePath)
+//	if err != nil {
+//		log.Fatal(fmt.Errorf("Failed to read HTML file: %v", err))
+//		return
+//	}
+//
+//	htmlString = string(htmlBytes)
+//	return
+//}
